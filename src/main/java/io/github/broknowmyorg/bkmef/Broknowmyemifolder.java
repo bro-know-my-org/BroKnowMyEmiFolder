acfbb8b8b8e69fb982d02b1e8492228f0da86741
JIA <430731352@qq.com>
2026-06-04 23:35:00 +0800

    chore: 初始化NeoForge构建骨架

---
+++ src/main/java/io/github/broknowmyorg/bkmef/Broknowmyemifolder.java
@@ -0,0 +1,11 @@
+package io.github.broknowmyorg.bkmef;
+
+import net.neoforged.fml.common.Mod;
+
+@Mod(Broknowmyemifolder.MODID)
+public class Broknowmyemifolder {
+    public static final String MODID = "broknowmyemifolder";
+
+    public Broknowmyemifolder() {
+    }
+}
